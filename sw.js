e4428731c512b0ce6423be6a730e215636705feb
横山 豪 <47491204+Yokoyama-go@users.noreply.github.com>
2020-06-22 11:05:50 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,60 @@
+const CACHE_NAME = 'cash';
+// Cache targets
+const urlsToCache = [
+  './index.html',
+  'style.css',
+  'script.js',
+  'snackbar.css',
+  'manifest.json',
+];
 
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((response) => {
+        return response ? response : fetch(event.request);
+      })
+  );
+});
+
+/*
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(
+        [
+          '/index.html',
+          '/css/main.css',
+          '/js/bootstrap.min.js',
+          '/js/jquery.min.js',
+          '/offline.html'
+        ]
+      );
+    })
+  );
+});*/
+
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        './index.html',
+        '/style.css',
+        '/script.js',
+        '/snackbar.css'
+      ]);
+    })
+  );
+});
